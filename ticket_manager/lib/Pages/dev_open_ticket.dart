408cbd890c03dda8cff6da725ac8554bc4ce0fb5
ZacharyNelson <nelsonza@hendrix.edu>
2020-11-13 12:10:02 -0600

    change the layout of code

--- ticket_manager/lib/Pages/dev_open_ticket.dart
+++ ticket_manager/lib/Pages/dev_open_ticket.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:ticket_manager/Objects/ticket.dart';
 import 'dev_ticket_page.dart';
 
-
-class OpenTicket extends StatefulWidget{
+class OpenTicket extends StatefulWidget {
   //Strings are temporary, will be replaced with ticket information
   final String a;
   final String b;
   final String c;
   OpenTicket(this.a, this.b, this.c);
-  OpenTick createState()=> OpenTick();
+  OpenTick createState() => OpenTick();
 }
 
-class OpenTick extends State<OpenTicket>{
+class OpenTick extends State<OpenTicket> {
   // Post holds the data from post in main.
   OpenTick();
   String dropdownValue = 'Open';
@@ -34,13 +33,12 @@ class OpenTick extends State<OpenTicket>{
                 DropdownButton(
                   value: dropdownValue,
                   icon: Icon(Icons.arrow_downward),
-                  underline: Container(
-                  ),
+                  underline: Container(),
                   onChanged: (String newValue) {
                     setState(() {
                       dropdownValue = newValue;
                     });
-                    },
+                  },
                   items: <String>['Open', 'Pending', 'Closed']
                       .map<DropdownMenuItem<String>>((String value) {
                     return DropdownMenuItem<String>(
@@ -49,9 +47,7 @@ class OpenTick extends State<OpenTicket>{
                     );
                   }).toList(),
                 ),
-               ]
-            ),
-        )
-    );
+              ]),
+        ));
   }
 }
