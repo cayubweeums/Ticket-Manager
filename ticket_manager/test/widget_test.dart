d281faee803c5d03cc3dd165a50565d94048c883
ZacharyNelson <nelsonza@hendrix.edu>
2020-12-04 20:24:58 -0600

    test case

--- ticket_manager/test/widget_test.dart
+++ ticket_manager/test/widget_test.dart
@@ -13,20 +13,15 @@ import 'package:ticket_manager/main.dart';
 
 import '../lib/Services/database.dart';
 
-
 void main() {
   test('Create Ticket Class Test', () {
     String author = "Test User";
     String title = "Test Title";
     String description = "Placeholder";
+    String longDescription = "Placeholder";
     String state = "Open";
     // ignore: non_constant_identifier_names
-    Ticket T1 = Ticket(
-        author,
-        title,
-        description,
-        state
-    );
+    Ticket T1 = Ticket(author, title, description, longDescription, state);
     expect(T1, T1);
   });
 
@@ -53,9 +48,9 @@ void main() {
     //Under Construction
     //String user = "Test User"; Firebase doesn't store usernames in the database when connectiong, unnecessary(?)
     AuthService _authService = AuthService();
-    expect(null, _authService.signInAnonymously()); // Expects a null as sigining in anonymously does not return specific usernames
+    expect(
+        null,
+        _authService
+            .signInAnonymously()); // Expects a null as sigining in anonymously does not return specific usernames
   });
-
-
-
 }
